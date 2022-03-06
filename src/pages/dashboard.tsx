c95c034e44bb9e775ba30cea07117748639fb3af
mariliamessias <marilia.oliveira@outlook.com>
2022-03-06 18:36:38 -0300

    create sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,26 @@
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
-export default function DashBoard(){
-    return(
-       <Header/>
-    )
+export default function DashBoard() {
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+      <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Inscritos da semana
+            </Text>
+          </Box>
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Taxa de abertura
+            </Text>
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Flex>
+  );
 }
